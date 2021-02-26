96b6a690f5cd574b9a1089d792cf72d8d3dd14ce
funnykoss <noanybody26@gmail.com>
2021-02-26 18:54:59 +0200

    start

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,17 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-mobile-open]'),
+    closeModalBtn: document.querySelector('[data-mobile-close]'),
+    modal: document.querySelector('[data-mobile]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
